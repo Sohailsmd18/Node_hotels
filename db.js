7c094f2e0fe94a49ce4e6e0201cbb5a0bff3bb8a
MyChat <sohailsmd18@gmail.com>
2024-09-01 18:34:47 +0530

    shift mongodb to online server

--- db.js
+++ db.js
@@ -1,7 +1,11 @@
 const mongoose=require('mongoose');
-//Define the mongoDB URL
-const mongoURL='mongodb://localhost:27017/hotels'
+require('dotenv').config();
 
+//Define the mongoDB URL
+//const mongoURL='mongodb://localhost:27017/hotels' //Local database
+//const mongoURL= //cloud database in env for password hiding
+//const mongoURL=process.env.MONGODB_URL;
+const mongoURL=process.env.MONGODB_URL_LOCAL;
 mongoose.connect(mongoURL,{
     useNewUrlParser:true,
     useUnifiedTopology:true
--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 const express=require('express');
 const app=express();
 const db=require('./db');
-
+require('dotenv').config();
 const bodyParser=require('body-parser');
 app.use(bodyParser.json());
 
 
+const PORT=process.env.PORT||3000;
 
 app.get('/',function(req,res){
     res.send('Welcome to our Hotel');
@@ -37,7 +38,7 @@ const menuItemRoutes=require('./routes/menuItemRoutes');
 app.use('/',personRoutes);
 app.use('/',menuItemRoutes);
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log("listening on port 3000")
 });
 //comment
