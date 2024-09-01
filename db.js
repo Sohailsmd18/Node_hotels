04b8c42439725203da88a84b1592df2da589447c
MyChat <sohailsmd18@gmail.com>
2024-09-01 18:46:40 +0530

    shift mongodb server online

--- db.js
+++ db.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 //Define the mongoDB URL
 //const mongoURL='mongodb://localhost:27017/hotels' //Local database
 //const mongoURL= //cloud database in env for password hiding
-//const mongoURL=process.env.MONGODB_URL;
-const mongoURL=process.env.MONGODB_URL_LOCAL;
+const mongoURL=process.env.MONGODB_URL;
+//const mongoURL=process.env.MONGODB_URL_LOCAL;
 mongoose.connect(mongoURL,{
     useNewUrlParser:true,
     useUnifiedTopology:true
